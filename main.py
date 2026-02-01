8dace9a522a308ec4f631073d098b23b6da5009b
Tueminh2003 <tueminhnguyen2705@gmail.com>
2026-02-02 01:16:29 +0700

    uploading my token

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ def main():
 
 if __name__ == "__main__":
     main()
+# my token is bc050b1dff53a2b4b667674ad5b2e7239b7318c4c3
+from speckle.api.client import SpeckleClient
+
+client = SpeckleClient(hosts="api.speckle.systems")
+
+token = "bc050b1dff53a2b4b667674ad5b2e7239b7318c4c3"
+client.authenticate_with_token(token)
+
+print (f" Authenticated as {client.account.userInfo.name}")
